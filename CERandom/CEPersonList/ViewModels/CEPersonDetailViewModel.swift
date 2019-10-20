77c0d9ba52cf1c7a2ee7b3fb8ec4294b9e254574
Zinuk <mdzinuk@yahoo.com>
2019-10-20 23:42:07 +1300

    Task2: Added detail view;

---
+++ CERandom/CEPersonList/ViewModels/CEPersonDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CEPersonDetailViewModel.swift
+//  CERandom
+//
+//  Created by Mohammad Arafat Hossain on 20/10/19.
+//  Copyright © 2019 Mohammad Arafat Hossain. All rights reserved.
+//
+
+import Foundation
